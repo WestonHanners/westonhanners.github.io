007e97748e39aeef0413213631945427c7141e3e
Weston Hanners <whanners@me.com>
2019-06-26 15:19:24 -0400

    compression and caching

--- config/config.php
+++ config/config.php
@@ -20,6 +20,7 @@ $config['site_title'] = '...alloc] init];';
 $config['banner_image'] = 'banner';
 $config['pages_order'] = 'meta.date:desc';
 $config['article_count'] = 4;
+$config['page_compress_level'] = 1;
 /**
  * default theme
  */
@@ -28,12 +29,12 @@ $config['theme'] = 'alloc';
 /**
  * Activate the persistent cache for better performance.
  */
-//$config['plugins']['phile\\phpFastCache'] = ['active' => true, 'storage' => 'files'];
+$config['plugins']['phile\\phpFastCache'] = ['active' => true, 'storage' => 'files'];
 
 /**
  * Use the demo-plugin as a starting point to write your own plugins.
  */
-//$config['plugins']['mycompany\\demoPlugin'] = ['active' => true];
+$config['plugins']['softr\\pageCompressor'] = ['active' => true];
 $config['plugins']['kronusdark\\pageCount'] = ['active' => true];
 $config['plugins']['infostreams\\snippets'] = ['active' => true];
 $config['plugins']['phile\\rssFeed'] = ['active' => true];
