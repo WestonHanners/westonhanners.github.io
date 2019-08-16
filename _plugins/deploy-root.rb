89772bbf3002f95189488c49b200f404939e1516
Weston Hanners <whanners@me.com>
2019-08-16 15:43:25 -0400

    Fixed plugin

--- _plugins/deploy-root.rb
+++ _plugins/deploy-root.rb
@@ -5,7 +5,7 @@ module Jekyll
   Jekyll::Hooks.register :site, :post_write do |post|
     Dir.glob('_root/*.*') do |file|
       sourcePath = file
-      outputPath = File.join('_site', File.basename(file) )
+      outputPath = File.join(configuration['destination'], File.basename(file) )
       FileUtils.cp(sourcePath, outputPath)
     end
   end
