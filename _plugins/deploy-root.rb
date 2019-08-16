76469948f1527cdaff032388158eae1e24d3783a
Weston Hanners <whanners@me.com>
2019-08-16 15:55:00 -0400

    fix root deploy plugin

--- _plugins/deploy-root.rb
+++ _plugins/deploy-root.rb
@@ -2,10 +2,10 @@
 require 'fileutils'
 
 module Jekyll
-  Jekyll::Hooks.register :site, :post_write do |post|
+  Jekyll::Hooks.register :site, :post_write do |site|
     Dir.glob('_root/*.*') do |file|
       sourcePath = file
-      outputPath = File.join(configuration['destination'], File.basename(file) )
+      outputPath = File.join(site.config['destination'], File.basename(file) )
       FileUtils.cp(sourcePath, outputPath)
     end
   end
