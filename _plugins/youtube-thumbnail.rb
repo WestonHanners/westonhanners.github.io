1aebce245896d293642ac132eadffbaeedb96f98
Weston Hanners <weston@Ragnarok.hsd1.in.comcast.net>
2019-12-05 17:03:29 -0500

    test plugin

--- _plugins/youtube-thumbnail.rb
+++ _plugins/youtube-thumbnail.rb
@@ -2,6 +2,10 @@
 
 module Jekyll
     Jekyll::Hooks.register :posts, :post_render do |post|
-        print(post)
+        post.gsub("<p><a href="https:\/\/www\.youtube\.com\/watch\?v=\w+">\w+<\/a><\/p>") do |match|
+            alt = match.match("(?<=alt=")(.*)(?=")")
+            videoID = match.match("(?<=\?v=)(.*)(?=">)")
+            return "<p><a href=\"https://www.youtube.com/watch?v=#{videoID}\"><img src="http://img.youtube.com/vi/#{videoID}/0.jpg\" alt=\"#{alt}\" /></a></p>"
+        end
     end
 end
