39056066495d3cb0be7b06a55f870cccc0b7eacb
Weston Hanners <weston@Ragnarok.hsd1.in.comcast.net>
2019-12-05 17:25:46 -0500

    testing escaping

--- _plugins/youtube-thumbnail.rb
+++ _plugins/youtube-thumbnail.rb
@@ -5,8 +5,8 @@ module Jekyll
         post.content.gsub((?=<p><a href=\"https:\/\/www\.youtube\.com\/watch\?v=).*(?:<\/p>)) do |match|
             print(post)
 
-            altText = match.match("(?<=\">).+(?=<\/a>)")
-            videoID = match.match("(?<=\?v=)(.*)(?=\">)")
+            altText = match.match((?<=\">).+(?=<\/a>))
+            videoID = match.match((?<=\?v=)(.*)(?=\">))
 
             "<p><a href=\"https://www.youtube.com/watch?v=#{videoID}\"><img src=\"http://img.youtube.com/vi/#{videoID}/0.jpg\" alt=\"#{altText}\" /></a></p>"
         end
