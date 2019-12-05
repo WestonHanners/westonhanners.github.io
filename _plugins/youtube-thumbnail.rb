cf96168125552aa388a8de292bd4c2cbf95f0c51
Weston Hanners <weston@Ragnarok.hsd1.in.comcast.net>
2019-12-05 17:04:24 -0500

    Fixed escape

--- _plugins/youtube-thumbnail.rb
+++ _plugins/youtube-thumbnail.rb
@@ -2,7 +2,7 @@
 
 module Jekyll
     Jekyll::Hooks.register :posts, :post_render do |post|
-        post.gsub("<p><a href="https:\/\/www\.youtube\.com\/watch\?v=\w+">\w+<\/a><\/p>") do |match|
+        post.gsub("<p><a href=\"https:\/\/www\.youtube\.com\/watch\?v=\w+\">\w+<\/a><\/p>") do |match|
             alt = match.match("(?<=alt=")(.*)(?=")")
             videoID = match.match("(?<=\?v=)(.*)(?=">)")
             return "<p><a href=\"https://www.youtube.com/watch?v=#{videoID}\"><img src="http://img.youtube.com/vi/#{videoID}/0.jpg\" alt=\"#{alt}\" /></a></p>"
