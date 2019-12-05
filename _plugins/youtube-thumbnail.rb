9d75c781bb059158f06b91466cb4c0397d484f20
Weston Hanners <weston@Ragnarok.hsd1.in.comcast.net>
2019-12-05 17:57:55 -0500

    maybe we need output

--- _plugins/youtube-thumbnail.rb
+++ _plugins/youtube-thumbnail.rb
@@ -2,13 +2,13 @@
 
 module Jekyll
     Jekyll::Hooks.register :posts, :post_render do |post|
-        newContent = post.content.gsub(/(?=<p><a href=\"https:\/\/www\.youtube\.com\/watch\?v=).*(?:<\/p>)/) { |match|
+        newOutput = post.output.gsub(/(?=<p><a href=\"https:\/\/www\.youtube\.com\/watch\?v=).*(?:<\/p>)/) { |match|
             altText = match.match(/(?<=\">).+(?=<\/a>)/)
             videoID = match.match(/(?<=\?v=)(.*)(?=\">)/)
 
             "<p><a href=\"https://www.youtube.com/watch?v=#{videoID}\"><img src=\"http://img.youtube.com/vi/#{videoID}/0.jpg\" alt=\"#{altText}\" /></a></p>"            
         }
 
-        post.content = newContent
+        post.output = newOutput
     end
 end
