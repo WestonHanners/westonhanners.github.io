cdc4d9a1d981bfb167c14f1477df51d780dbb031
Weston Hanners <weston@ragnarok.lan>
2019-12-09 11:47:54 -0500

    Typo

--- _plugins/youtube-thumbnail.rb
+++ _plugins/youtube-thumbnail.rb
@@ -2,13 +2,13 @@
 
 module Jekyll
     Jekyll::Hooks.register :posts, :post_render do |post|
-        
+
         newOutput = post.output.gsub(/(?=<p><a href=\"https:\/\/www\.youtube\.com\/watch\?v=).*(?:<\/p>)/) { |match|
 
             altText = match.match(/(?<=\">).+(?=<\/a>)/)
             videoID = match.match(/(?<=\?v=)(.*)(?=\">)/)
 
-            "<p><a href=\"https://www.youtube.com/watch?v=#{videoID}\"><img src=\"http://img.youtube.com/vi/#{videoID}/0.jpg\" alt=\"#{altText}\" /></a></p>"            
+            "<p><a href=\"https://www.youtube.com/watch?v=#{videoID}\"><img src=\"https://img.youtube.com/vi/#{videoID}/0.jpg\" alt=\"#{altText}\" /></a></p>"            
         }
 
         post.output = newOutput
